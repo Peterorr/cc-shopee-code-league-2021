a4eefa6ff2bbcb4351768821c71de6a01fd937e9
Sam <sam7k9621@gmail.com>
2021-03-11 01:48:10 +0800

    Fix bug: overlap identical list issue

--- notebooks/inverted_index.py
+++ notebooks/inverted_index.py
@@ -2,51 +2,50 @@ import pandas as pd
 import csv
 def main():
     df = pd.read_json("../data/raw/contacts.json")
+
     id_hash, label_hash = {}, {}
-
     for idx, row in df.iterrows():
-        
-        # Get contact value before dropping it
-        contact = row[3]
-
         # Drop useless info
         row = row.drop(labels=['Id', 'Contacts'])
-       
         # Store label list as [ Email, Phone, OrderId ]
         label_lst =  list(filter( None, row.tolist() ))
         id_hash[ idx ] = label_lst
         
-        # Assign same list object to same person in label_hash
-        common_contact_lst, common_label_lst = [], []
+        # Assign identical set object to same person in label_hash
+        common_label_set = set()
         for label in label_lst:
             try:
-                common_contact_lst, common_label_lst = label_hash[ label ]
-            except:
-                # Recreate label hash if not found
-                label_hash[ label ] = common_contact_lst, common_label_lst
+                temp = label_hash[ label ]
+                if common_label_set:
+                    if common_label_set is not temp:
+                        common_label_set |= temp
+                        for i in common_label_set:
+                            for l in id_hash[ i ]:
+                                label_hash[ l ] = common_label_set
+                else:
+                    common_label_set = label_hash[ label ]
+            
+            # Create label hash if not found
+            except KeyError:
+                label_hash[ label ] = common_label_set
         
         # Update list object in label_hash
-        common_label_lst.append( idx )
-        common_contact_lst.append( contact )
+        common_label_set.add( idx )
         for label in label_lst:
-            label_hash[ label ] = common_contact_lst, common_label_lst
+            label_hash[ label ] = common_label_set
 
     # Output csv file
     with open('test.csv', "w") as csvfile:
         csvwriter = csv.writer(csvfile)
         csvwriter.writerow( ['ticket_id', 'ticket_trace/contact'] )
-        
+       
         for idx, row in df.iterrows():
-            contactlst, tracelst = label_hash[ id_hash[ idx ][0] ]
+            tracelst = sorted( list( label_hash[ id_hash[ idx ][0] ] ) )
+            # Retrieve contact value (could be improved?)
+            contactlst = [ df.iloc[x][3] for x in tracelst]
+            
             tracelst = [str(x) for x in tracelst]
             csvwriter.writerow( [idx, "{}, {}".format( "-".join( tracelst), sum(contactlst) ) ]  )
-    
-   
-    
-    
-
-
-
 
 if __name__ == '__main__':
     main()
